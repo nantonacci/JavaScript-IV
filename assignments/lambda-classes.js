d466ca239412ecea6181c4f579831facd7a39fc2
Nicole Antonacci <nicole.antonacci@gmail.com>
2019-05-30 19:15:37 -0700

    Completed lambda classes assignment with stretch.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -11,7 +11,7 @@ class Person {
   }
 }
 
-class Instructors extends Person {
+class Instructor extends Person {
   constructor(attributes) {
     super(attributes);
     this.specialty = attributes.specialty;
@@ -21,32 +21,46 @@ class Instructors extends Person {
   demo(subject) {
     console.log(`Today we are learning about ${subject}`);
   }
-  grade(student) {
+  grade(student, subject) {
     console.log(`${student.name} receives a perfect score on ${subject}`);
+  }
+  changeGrade(student) {
+    let value = Math.floor(Math.random() * 100) + 1;
+    console.log(student.grades + value);
   }
 }
 
-class Students extends Person {
+class Student extends Person {
   constructor(attributes) {
     super(attributes);
     this.previousBackground = attributes.previousBackground;
     this.className = attributes.className;
     this.favSubjects = attributes.favSubjects;
+    this.grades = attributes.grades;
   }
   listsSubjects() {
-    console.log(`all of the student's favoriteSubjects one by one`);
+    this.favSubjects.forEach(function(item) {
+      console.log(item);
+    });
+  }
+  PRAssignment(subject) {
+    console.log(`${this.name} has submitted a PR for ${subject}`);
   }
 
-  PRAssignment() {
-    console.log(`${student.name} has submitted a PR for ${subject}`);
+  sprintChallenge(subject) {
+    console.log(`${this.name} has begun sprint challenge on ${subject}`);
   }
 
-  sprintChallenge() {
-    console.log(`${student.name} has begun sprint challenge on ${subject}`);
+  graduate() {
+    if (this.grades > 70) {
+      console.log(`Congrats, grad!`);
+    } else {
+      console.log(`Keep studying!`);
+    }
   }
 }
 
-class ProjectManagers extends Instructors {
+class ProjectManager extends Instructor {
   constructor(attributes) {
     super(attributes);
     this.gradClassName = attributes.gradClassName;
@@ -55,11 +69,61 @@ class ProjectManagers extends Instructors {
 
   standUp(slackChannel) {
     console.log(
-      `${name} announces to ${slackChannel}, @channel standy times!​​​​​`
+      `${this.name} announces to ${slackChannel}, @channel standy times!​​​​​`
     );
   }
 
-  debugsCode(studentObj, subject) {
-    console.log(`${name} debugs ${student.name}'s code on ${subject}​​​​​`);
+  debugsCode(student, subject) {
+    console.log(
+      `${this.name} debugs ${student.name}'s code on ${subject}​​​​​`
+    );
   }
 }
+
+const jon = new Person({ name: 'Jon', age: 25, location: 'Poughkeepsie' });
+console.log(jon);
+jon.speak();
+
+const pavithra = new Student({
+  name: 'Pavithra',
+  age: 29,
+  location: 'Renton',
+  previousBackground: 'Medical Billing Specialist',
+  className: 'WebPT7',
+  favSubjects: ['HTML', 'CSS', 'JavaScript', 'Python'],
+  grades: 80
+});
+console.log(pavithra);
+pavithra.listsSubjects();
+pavithra.PRAssignment('JavaScript');
+pavithra.sprintChallenge('JavaScript');
+console.log(pavithra.grades);
+
+const sally = new Instructor({
+  name: 'Sally',
+  age: 37,
+  location: 'Alamo',
+  specialty: 'JavaScript',
+  favLanguage: 'JavaScript',
+  catchPhrase: `JavaScript? More like LavaScript! Cause it's hot! ...right guys?`
+});
+console.log(sally);
+sally.demo('LavaScript');
+sally.grade(pavithra, 'JavaScript');
+sally.changeGrade(pavithra);
+pavithra.graduate();
+const sato = new ProjectManager({
+  name: 'Sato',
+  age: 40,
+  location: 'Boseman',
+  specialty: 'Front End',
+  favLanguage: 'CSS',
+  catchPhrase: `Don't be afraid! CSS is your friend!`,
+  gradClassName: 'Web42',
+  favInstructor: 'Sally'
+});
+console.log(sato);
+sato.standUp('webpt7_sato');
+sato.debugsCode(pavithra, 'CSS');
+sato.demo('Flexbox');
+sally.grade(pavithra, 'Flexbox');
